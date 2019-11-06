5b9603568028ad10343a7ea3e1a6ffe8965348f8
EDGART <edgar@getpoints.com>
2019-11-06 17:44:41 +0300

    changes to ctegory component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -7,8 +7,42 @@ function Categories(props){
             <Category 
                 name="Restaurants"
                 source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
-                link="./restaurants"
-                
+                link="./restaurants"                
+            />
+            <Category 
+                name="Shopping"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_shopping.yji-ad1f85392c049e567b6ee9dd50360ba4.png"
+                link="./restaurants"                
+            />
+            <Category 
+                name="Night life"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_nightlife.yji-4ee31d03d5b20dc5575588c3d284dc2d.png"
+                link="./restaurants"                
+            />
+            <Category 
+                name="Active Life"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
+                link="./restaurants"                
+            />
+            <Category 
+                name="Beauty Spas"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
+                link="./restaurants"                
+            />
+            <Category 
+                name="Automotive"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
+                link="./restaurants"                
+            />
+            <Category 
+                name="Home Services"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
+                link="./restaurants"                
+            />
+            <Category 
+                name="More Categories"
+                source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
+                link="./restaurants"                
             />
         </div>
     )
--- src/components/Category.js
+++ src/components/Category.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Category(props){
     return(
-        <a href={props.link}>
+        <a href={props.link} className="category">
             <img src={props.source} alt={props.description} />  
             {props.name}   
         </a>
