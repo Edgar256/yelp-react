7b2d9a26ead5436176dc5a72d9c52aa9ac76c220
EDGART <edgar@getpoints.com>
2019-11-07 12:27:11 +0300

    responsive fixes for the home page

--- src/App.js
+++ src/App.js
@@ -32,9 +32,10 @@ function App() {
             </select>
 
             <input type="text" className="form-control custom-select" aria-label="Text input with segmented dropdown button"></input>
-            <div className="input-group-prepend">
-              <label className="input-group-text btn-search" for="inputGroupSelect01">
-                <svg id="24x24_search" height="24" viewBox="0 0 24 24" width="24"><path d="M20.753 19.34l-4.295-4.297A7.46 7.46 0 0 0 18 10.5a7.5 7.5 0 1 0-7.5 7.5 7.46 7.46 0 0 0 4.543-1.542l4.296 4.295a1 1 0 1 0 1.412-1.414zM10.5 16A5.506 5.506 0 0 1 5 10.5C5 7.467 7.467 5 10.5 5S16 7.467 16 10.5 13.533 16 10.5 16z"></path></svg>
+            <div className="input-group-prepend btn-search-wrapper" >
+              <label className="input-group-text btn-search btn-search-main" for="inputGroupSelect01">
+                <div className="searchText">Search</div>
+                <svg id="search-svg" height="24" viewBox="0 0 24 24" width="24"><path d="M20.753 19.34l-4.295-4.297A7.46 7.46 0 0 0 18 10.5a7.5 7.5 0 1 0-7.5 7.5 7.46 7.46 0 0 0 4.543-1.542l4.296 4.295a1 1 0 1 0 1.412-1.414zM10.5 16A5.506 5.506 0 0 1 5 10.5C5 7.467 7.467 5 10.5 5S16 7.467 16 10.5 13.533 16 10.5 16z"></path></svg>
               </label>
             </div>
           </div> 
