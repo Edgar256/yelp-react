de0408aa38128208cd058b8f3422faea76817af0
edgar256 <tinka.edgar@gmail.com>
2019-11-07 06:28:57 +0300

    added svg icons t o the home page

--- src/components/HomeIconLink.js
+++ src/components/HomeIconLink.js
@@ -3,8 +3,10 @@ import React from 'react';
 function HomeIconLink(props){
     return(
         <a href="#" className="home-icon-link">
-              {props.svg}
-              {props.name}
+              <svg id="18x18_delivery" height="18" viewBox="0 0 18 18" width="18">
+                  <path fill="#fff" d={props.svg}></path>
+              </svg>
+              <span>{props.name}</span>
         </a>
     )
 }
