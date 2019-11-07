a5ae2f2f0f512838f9720288f9abe6d965264300
EDGART <edgar@getpoints.com>
2019-11-07 17:43:55 +0300

    adding routing

---
+++ src/pages/restaurantsListing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function restaurantsListing(){
+    return(
+        <div>
+            Restaurants Listings page
+        </div>
+    )
+}
+
+export default restaurantsListing;
