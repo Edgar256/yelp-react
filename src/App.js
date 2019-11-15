5ce705dd0f88209f7308a696ec654119b34b686e
EDGART <edgar@getpoints.com>
2019-11-15 14:32:12 +0300

    added 7 category pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import {BrowserRouter as Router,
 import './App.css';
 import Home from './pages/Home';
 import RestaurantsListing from './pages/RestaurantsListing';
+import ShoppingListing from './pages/ShoppingListing';
+import NightLifeListing from './pages/NightLifeListing';
+import ActiveLifeListing from './pages/ActiveLifeListing';
+import BeautySpasListing from './pages/BeautySpasListing';
+import AutomotiveListing from './pages/AutomotiveListing';
+import HomeServicesListing from './pages/HomeServicesListing';
 import Error from './pages/Error';
 
 function App() {
@@ -15,9 +21,14 @@ function App() {
         <Switch>
           <Route path="/" component={Home} exact />
 					<Route path="/restaurants" component={RestaurantsListing} />
+          <Route path="/shopping" component={ShoppingListing} />
+          <Route path="/night-life" component={NightLifeListing} />
+          <Route path="/active-life" component={ActiveLifeListing} />
+          <Route path="/beauty-spas" component={BeautySpasListing} />
+          <Route path="/automotive" component={AutomotiveListing} />
+          <Route path="/home-services" component={HomeServicesListing} />
           <Route component={Error} />
         </Switch>
-
     </Router>
   );
 }
---
+++ src/pages/ShoppingListing.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import MapListing from '../components/MapListing';
+import BizCard from '../components/BizCard';
+import Footer from '../components/Footer';
+
+function ShoppingListing(){
+    return(
+        <div>
+            <div className="container">
+            <div className="row">
+                <div className="col-sm-9">
+                    <h3>The Best 10 Shopping Places in San Francisco, CA</h3>
+                    <div>
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                        <BizCard 
+                            name="Serena Hotels"
+                            category="Restaurants"
+                            reviews="243"
+                            reviewText="“I tried Kung Food for delivery yesterday evening and was pleasantly surprised. Delivery was on time and the selections arrive hot. Highly recommend their hot…” more"
+                            pricing="$$"
+                            source="https://www.serenahotels.com/d/serenakampala/media/__thumbs_306_300_crop/bedroom.jpg"
+                            tel="(415) 673-6688"
+                            street="1615 McAllister St"
+                            location="Noa Poa"
+                        />
+                    </div>
+                </div>                
+                <div className="col-sm-3">
+                    <MapListing />                        
+                </div>
+            </div> 
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default ShoppingListing;
