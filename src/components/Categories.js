369ccde5550b6aa09089bb370dd5dcde36ba24b2
EDGART <edgar@getpoints.com>
2019-11-26 17:32:20 +0300

    linl fix

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -9,7 +9,7 @@ function Categories(props){
                 <Category 
                     name="Restaurants"
                     source="https://s3-media0.fl.yelpcdn.com/assets/public/72x72_restaurants.yji-80b92cd513f0cd02dd41a05d5d846075.png"
-                    link="restaurants"                
+                    link="/restaurants"                
                 />
                 <Category 
                     name="Shopping"
--- src/components/Category.js
+++ src/components/Category.js
@@ -3,11 +3,10 @@ import {Link} from 'react-router-dom'
 
 function Category(props){
     return(
-        <Link to="/{props.link} "className="category">
+        <a href={props.link} className="category">
             <img src={props.source} alt={props.description} className="category-image"/>  
             {props.name}   
-            <Link to="/{props.link}"></Link>
-        </Link>
+        </a>
     )
 }
 
