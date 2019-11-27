f783aff3029090ed44e85aa6c59856511a4b0871
EDGART <edgar@getpoints.com>
2019-11-27 18:06:58 +0300

    worked partially in the Router

--- src/App.js
+++ src/App.js
@@ -13,8 +13,35 @@ import ActiveLifeListing from './pages/ActiveLifeListing';
 import BeautySpasListing from './pages/BeautySpasListing';
 import AutomotiveListing from './pages/AutomotiveListing';
 import HomeServicesListing from './pages/HomeServicesListing';
-import Error from './pages/Error';
 import WriteAReview from './pages/WriteAReview';
+import About from './pages/About';
+import Careers from './pages/Careers';
+import Press from './pages/Press';
+import InvestorRelations from './pages/InvestorRelations';
+import ContentGuidelines from './pages/ContentGuidelines';
+import TermsOfService from './pages/TermsOfService';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import AdChoices from './pages/AdChoices';
+import YelpProjectCostGuides from './pages/YelpProjectCostGuides';
+import Collections from './pages/Collections';
+import Talk from './pages/Talk';
+import Events from './pages/Events';
+import TheLocalYelp from './pages/TheLocalYelp';
+import YelpBlog from './pages/YelpBlog';
+import Support from './pages/Support';
+import YelpMobile from './pages/YelpMobile';
+import Developers from './pages/Developers';
+import RSS from './pages/RSS';
+import YelpForBusinessOwners from './pages/YelpForBusinessOwners';
+import ClaimYourBusinessPage from './pages/ClaimYourBusinessPage';
+import AdvertiseOnYelp from './pages/AdvertiseOnYelp';
+import YelpReservations from './pages/YelpReservations';
+import YelpWifi from './pages/YelpWifi';
+import YelpWaitList from './pages/YelpWaitList';
+import BusinessSuccessStories from './pages/BusinessSuccessStories';
+import BusinessSupport from './pages/BusinessSupport';
+import YelpBlogForBusinessOwner from './pages/YelpBlogForBusinessOwner';
+import Error from './pages/Error';
 
 function App() {
   return (
@@ -29,6 +56,24 @@ function App() {
           <Route path="/automotive" component={AutomotiveListing} />
           <Route path="/home-services" component={HomeServicesListing} />
           <Route path="/write-a-review" component={WriteAReview}/>
+          <Route path="/about-yelp" component={About}/>
+          <Route path="/careers" component={Careers}/>
+          <Route path="/press" component={Press}/>
+          <Route path="/investor-relations" component={InvestorRelations} />
+          <Route path="/content-guidelines" component={ContentGuidelines} />
+          <Route path="/terms-of-service" component={TermsOfService} />
+          <Route path="/privacy-policy" component={PrivacyPolicy} />
+          <Route path="/ad-choices" component={AdChoices}/>
+          <Route path="/yelp-project-cost-guides" component={YelpProjectCostGuides} />
+          <Route path="/collections" component={Collections} />
+          <Route path="/talk" component={Talk} />
+          <Route path="/events" component={Events} />
+          <Route path="/the-local-yelp" component={TheLocalYelp} />
+          <Route path="/yelp-blog" component={YelpBlog} />
+          <Route path="/support" component={Support} />
+          <Route path="/yelp-mobile" component={YelpMobile} />
+          <Route path="/developers" component={Developers} />
+          <Route path="/rss" component={RSS} />
           <Route component={Error} />
         </Switch>
     </Router>
